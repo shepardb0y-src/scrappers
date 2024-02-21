d5d378c1170a059a88838ebadc302101808cc79a
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2024-02-21 16:02:34 -0500

    I added a guard clause within th link model to ensure the admin page renders data even if the links do not have any data

--- webscrapper/myapp/models.py
+++ webscrapper/myapp/models.py
@@ -6,7 +6,14 @@ from django.db import models
 class Link(models.Model):
 
     def __str__(self):
-        return self.name
+        if self.address and self.name:  # Check if both fields are not None
+            return f"{self.address} - {self.name}"
+        elif self.address:  # Check if only field1 is not None
+            return self.address
+        elif self.name:  # Check if only field2 is not None
+            return self.name
+        else:
+            return "Unnamed Object"
 
     address = models.CharField(max_length=1000, null=True, blank=True)
     name = models.CharField(max_length=1000, null=True, blank=True)
