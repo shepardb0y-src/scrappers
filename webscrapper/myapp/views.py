7c8028d74d8fe4f04127b4cb91fabfbde3941f1b
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2024-02-21 14:26:17 -0500

    i was able to connect to google and scrape thier anchor tags

--- webscrapper/myapp/views.py
+++ webscrapper/myapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape(request):
+    page = requests.get('https://www.google.com')
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    link_address = []
+
+    for link in soup.find_all('a'):
+        link_address.append(link.get('href'))
+
+    return render(request, 'myapp/result.html', {'link_address': link_address})
